6f678a91a685f86fdf52ac6dea1c5536a7d0ab32
jerryzejia <jerryzejia@gmail.com>
2017-05-27 21:50:11 -0400

    Fixed focus and toolbar problem
    bug fix

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -11,16 +11,24 @@ import com.github.alinz.reactnativewebviewbridge.WebViewBridgePackage;
 import java.util.Arrays;
 import java.util.List;
 
+import com.facebook.react.modules.network.ReactCookieJarContainer;
+import com.facebook.stetho.Stetho;
+import okhttp3.OkHttpClient;
+import com.facebook.react.modules.network.OkHttpClientProvider;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+import java.util.concurrent.TimeUnit;
+
+
 public class MainApplication extends Application implements ReactApplication {
 
     private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
         @Override
-        protected boolean getUseDeveloperSupport() {
+        public boolean getUseDeveloperSupport() {
             return BuildConfig.DEBUG;
         }
 
         @Override
-        protected List<ReactPackage> getPackages() {
+        public List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
                     new WebViewBridgePackage()
@@ -32,4 +40,18 @@ public class MainApplication extends Application implements ReactApplication {
     public ReactNativeHost getReactNativeHost() {
         return mReactNativeHost;
     }
+
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(0, TimeUnit.MILLISECONDS)
+                .readTimeout(0, TimeUnit.MILLISECONDS)
+                .writeTimeout(0, TimeUnit.MILLISECONDS)
+                .cookieJar(new ReactCookieJarContainer())
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
+        OkHttpClientProvider.replaceOkHttpClient(client);
+    }
+
 }
